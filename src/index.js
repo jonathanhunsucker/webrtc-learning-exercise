388cf1748cde407df019f94f42ee66fc954769e3
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-05-18 19:22:42 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return <p>Hello, world!</p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
